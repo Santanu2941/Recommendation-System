b3a57f7e5247558df7540656760d812cfad04214
Santanu Ghosh <131514748+Santanu2941@users.noreply.github.com>
2023-08-12 14:25:20 +0530

    Update BookRecommendationSystem.java

--- BookRecommendationSystem.java
+++ BookRecommendationSystem.java
@@ -6,8 +6,8 @@ public class BookRecommendationSystem {
 
     static {
         // Initialize the book similarities map with some data
-        bookSimilarities.put("The Lord of the Rings", Arrays.asList("The Hobbit", "The Silmarillion", "The Chronicles of Narnia"));
-        bookSimilarities.put("The Hunger Games", Arrays.asList("Divergent", "Maze Runner", "The Selection"));
+        bookSimilarities.put("The Adventures of Huckleberry Finn", Arrays.asList("The Hobbit", "The Silmarillion", "The Chronicles of Narnia"));
+        bookSimilarities.put("Middlemarch", Arrays.asList("Divergent", "Maze Runner", "The Selection"));
         bookSimilarities.put("To Kill a Mockingbird", Arrays.asList("The Catcher in the Rye", "1984", "Animal Farm"));
     }
 
